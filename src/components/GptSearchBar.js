e8a0d91e1ce9a854617cbede3e596579bfa1c34c
Bhupesh-aher <aherbhupesh18@gmail.com>
2024-10-27 01:08:39 +0530

    add mobile responsive search bar

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -66,14 +66,14 @@ const GptSearchBar = () => {
     }
 
     return (
-         <div className="pt-[35%] md:pt-[10%] flex justify-center">
-            <form className="w-full md:w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
+         <div className="pt-[40%] md:pt-[10%] flex justify-center">
+            <form className="w-11/12 md:w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
                 <input 
                 ref={searchText}
                 className="p-4 m-4 col-span-9" type="text" 
                 placeholder={lang[langKey].gptSearchPlaceholder}/>
 
-                <button className="py-2 px-4 m-4 col-span-3 bg-red-700 text-white rounded-lg"
+                <button className="px-1 md:py-2 px-4 m-4 col-span-3 bg-red-700 text-white rounded-lg"
                 onClick={handleGptSearchClick}>
                     {lang[langKey].search}
                     </button>
